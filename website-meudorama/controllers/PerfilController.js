9c2bdfb6ffc5dae703280be0c312107c95d474d4
Natasha <natasha.pedroso18@gmail.com>
2026-04-13 11:11:11 -0300

    Atividade Final

--- website-meudorama/controllers/PerfilController.js
+++ website-meudorama/controllers/PerfilController.js
@@ -2,8 +2,22 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/perfil", function(req, res) {
-    res.render("perfil");
+router.get("/perfil", function(req, res) { 
+
+    const perfil = [
+        {
+            nome: "Natasha Pedroso da Silva",
+            data_nasc: "07 de Abril de 2000",
+            genero: "Comédia Romântica",
+            localizacao: "Registro, São Paulo",
+            usuario: "@natasha_dorameira",
+            imagem: "/imgs/profile.jpg",
+            email: "natasha@gmail.com"
+        }
+    ];
+    res.render("perfil", {
+        perfil : perfil
+    });
 });
 
 export default router;
